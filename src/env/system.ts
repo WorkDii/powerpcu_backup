9650af5367f327e01c3be084ea8fded3da869221
usman kareemee <usman2535@gmail.com>
2024-09-15 14:06:18 +0700

    refactor: Update system.ts to include production environment variables

--- src/env/system.ts
+++ src/env/system.ts
@@ -1,5 +1,6 @@
 import { load } from "@std/dotenv";
 const env = await load();
+const envProduction = await load({ envPath: "production.env" });
 import { systemSchema } from "./schema.ts";
 
-export default systemSchema.parse(env);
+export default systemSchema.parse({ ...env, ...envProduction });
